d60ee41d6aa1dbe5d65d2e84daf998951c9dc103
Dave Newbold <dave.newbold@cern.ch>
2017-05-11 16:02:59 +0200

    Cleaning up scripts

--- projects/master/software/config.py
+++ projects/master/software/config.py
@@ -6,7 +6,7 @@ import time
 
 uhal.setLogLevelTo(uhal.LogLevel.NOTICE)
 manager = uhal.ConnectionManager("file://connections.xml")
-hw_list = [manager.getDevice("DUNE_FMC_MASTER")]
+hw_list = [manager.getDevice(i) for i in sys.argv[1:]]
 
 for hw in hw_list:
 
@@ -14,7 +14,7 @@ for hw in hw_list:
 
     m_v = hw.getNode("master.global.version").read()
     hw.dispatch()
-    print "Version:", hex(m_v)
+    print "FW version:", hex(m_v)
 
     m_t = hw.getNode("master.global.tstamp").readBlock(2)
     m_stat = hw.getNode("master.global.csr.stat").read()
@@ -22,14 +22,20 @@ for hw in hw_list:
     print "m_ts / m_stat:", hex(int(m_t[0]) + (int(m_t[1]) << 32)), hex(m_stat)
 
 	hw.getNode("master.partition.csr.ctrl.part_en").write(1) # Enable partition 0
-    hw.getNode("master.partition.csr.ctrl.buf_en").write(1) # Enable buffer in partition 0
+    hw.getNode("master.partition.csr.ctrl.buf_en").write(0) # Disable buffer in partition 0
     hw.getNode("master.partition.csr.ctrl.cmd_mask").write(0x000f) # Set command mask in partition 0
     hw.dispatch()
 
-    time.sleep(4)
+    time.sleep(1) # Allow endpoint time to sync
 
     hw.getNode("master.scmd_gen.ctrl.en").write(1) # Enable sync command generators
-    hw.getNode("master.scmd_gen.chan_ctrl.type").write(3) # Set type=3 for generator 0
+    hw.dispatch()
+    
+    hw.getNode("master.scmd_gen.chan_ctrl.type").write(1) # Set command type = 1 for generator 0
+    hw.getNode("master.scmd_gen.chan_ctrl.force").write(1) # Issue a single command
+    hw.dispatch()
+    
+    hw.getNode("master.scmd_gen.chan_ctrl.type").write(3) # Set command type = 3 for generator 0
     hw.getNode("master.scmd_gen.chan_ctrl.rate_div").write(0xe) # Set about 1Hz rate for generator 0
     hw.getNode("master.scmd_gen.chan_ctrl.patt").write(1) # Set Poisson mode for generator 0
     hw.getNode("master.scmd_gen.chan_ctrl.en").write(1) # Start the command stream
