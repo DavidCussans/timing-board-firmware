eec9f34797ca1f5f93175431bb0f87fc7ac40467
Dave Newbold <dave.newbold@cern.ch>
2017-05-19 08:38:38 +0100

    Bug fix in buffer; version number update

--- projects/endpoint/firmware/hdl/wrapper_defs.vhd
+++ projects/endpoint/firmware/hdl/wrapper_defs.vhd
@@ -9,7 +9,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 package wrapper_defs is
 
-	constant WRAPPER_VERSION: std_logic_vector(31 downto 0) := X"00000302"; -- Version number
+	constant WRAPPER_VERSION: std_logic_vector(31 downto 0) := X"00000303"; -- Version number
 	constant N_FIFO: integer := 1;
 	
 end wrapper_defs;
