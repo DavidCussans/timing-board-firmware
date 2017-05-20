30f74e303b31a8b05c9bff7efd7840f788612d76
Dave Newbold <dave.newbold@cern.ch>
2017-05-20 09:27:26 +0100

    Bug fix in master script

--- projects/master/software/config.py
+++ projects/master/software/config.py
@@ -22,7 +22,8 @@ for hw in hw_list:
     hw.dispatch()
     print "m_ts / m_stat:", hex(int(m_t[0]) + (int(m_t[1]) << 32)), hex(m_stat)
 
-    hw.getNode("master.partition.csr.ctrl.part_en").write(1) # Enable partition 0
+    hw.getNode("master.partition.csr.ctrl.trig_en").write(1)
+	hw.getNode("master.partition.csr.ctrl.part_en").write(1) # Enable partition 0
     hw.getNode("master.partition.csr.ctrl.buf_en").write(0) # Disable buffer in partition 0
     hw.getNode("master.partition.csr.ctrl.cmd_mask").write(0x000f) # Set command mask in partition 0
     hw.dispatch()
