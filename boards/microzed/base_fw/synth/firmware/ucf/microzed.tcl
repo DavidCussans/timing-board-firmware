e597496f28f72129ab1ed235c4b859fbb8a45e46
Dave Newbold <dave.newbold@cern.ch>
2018-01-20 18:27:30 +0000

    Adding reflector design

--- boards/microzed/base_fw/synth/firmware/ucf/microzed.tcl
+++ boards/microzed/base_fw/synth/firmware/ucf/microzed.tcl
@@ -13,43 +13,22 @@ proc false_path {patt clk} {
 	}
 }
 
-# System clock (200MHz)
-create_clock -period 20.000 -name sysclk [get_ports sysclk]
+# System clock (50MHz)
+create_clock -period 20.000 -name sysclk [get_ports sysclk_p]
 
-set_false_path -through [get_pins infra/clocks/rst_reg/Q]
-set_false_path -through [get_nets infra/clocks/nuke_i]
+# System clock (250MHz)
+create_clock -period 4.000 -name clk [get_ports clk_in_p]
 
-set_property IOSTANDARD LVCMOS25 [get_ports sysclk]
-set_property PACKAGE_PIN P17 [get_ports sysclk]
-
-set_property IOSTANDARD LVCMOS25 [get_ports {leds[*]}]
-set_property SLEW SLOW [get_ports {leds[*]}]
-set_property PACKAGE_PIN M16 [get_ports {leds[0]}]
-set_property PACKAGE_PIN M17 [get_ports {leds[1]}]
-set_property PACKAGE_PIN L18 [get_ports {leds[2]}]
-set_property PACKAGE_PIN M18 [get_ports {leds[3]}]
-false_path {leds[*]} sysclk
-
-set_property IOSTANDARD LVCMOS25 [get_ports {rgmii_* phy_rstn}]
-set_property PACKAGE_PIN R18 [get_ports {rgmii_txd[0]}]
-set_property PACKAGE_PIN T18 [get_ports {rgmii_txd[1]}]
-set_property PACKAGE_PIN U17 [get_ports {rgmii_txd[2]}]
-set_property PACKAGE_PIN U18 [get_ports {rgmii_txd[3]}]
-set_property PACKAGE_PIN T16 [get_ports {rgmii_tx_ctl}]
-set_property PACKAGE_PIN N16 [get_ports {rgmii_txc}]
-set_property PACKAGE_PIN U16 [get_ports {rgmii_rxd[0]}]
-set_property PACKAGE_PIN V17 [get_ports {rgmii_rxd[1]}]
-set_property PACKAGE_PIN V15 [get_ports {rgmii_rxd[2]}]
-set_property PACKAGE_PIN V16 [get_ports {rgmii_rxd[3]}]
-set_property PACKAGE_PIN R16 [get_ports {rgmii_rx_ctl}]
-set_property PACKAGE_PIN T14 [get_ports {rgmii_rxc}]
-set_property PACKAGE_PIN M13 [get_ports {phy_rstn}]
-false_path {phy_rstn} sysclk
-
-set_property IOSTANDARD LVCMOS25 [get_ports {cfg[*]}]
-set_property PULLUP TRUE [get_ports {cfg[*]}]
-set_property PACKAGE_PIN K2 [get_ports {cfg[0]}]
-set_property PACKAGE_PIN K1 [get_ports {cfg[1]}]
-set_property PACKAGE_PIN J4 [get_ports {cfg[2]}]
-set_property PACKAGE_PIN H4 [get_ports {cfg[3]}]
-
+set_property IOSTANDARD LVDS_25 [get_ports {sysclk_* clk_in_* d_in_* clk_out_* d_out_*}]
+set_property DIFF_TERM TRUE [get_ports {sysclk_* clk_in_* d_in_*}]
+set_property PACKAGE_PIN T9 [get_ports sysclk_p]
+set_property PACKAGE_PIN U10 [get_ports sysclk_n]
+set_property PACKAGE_PIN U18 [get_ports clk_in_p]
+set_property PACKAGE_PIN U19 [get_ports clk_in_n]
+set_property PACKAGE_PIN U14 [get_ports d_in_p]
+set_property PACKAGE_PIN U15 [get_ports d_in_n]
+set_property PACKAGE_PIN N20 [get_ports clk_out_p]
+set_property PACKAGE_PIN P20 [get_ports clk_out_n]
+set_property PACKAGE_PIN N18 [get_ports d_out_p]
+set_property PACKAGE_PIN P19 [get_ports d_out_n]
+false_path {d_in_* d_out_*} sysclk
---
+++ projects/test/reflect/firmware/hdl/reflect.vhd
@@ -0,0 +1,104 @@
+-- Simple reflector for PRBS test data
+--
+-- Dave Newbold, 14/1/18
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+entity top is port(
+		sysclk_p: in std_logic;
+		sysclk_n: in std_logic;
+		clk_in_p: in std_logic;
+		clk_in_n: in std_logic;
+		d_in_p: in std_logic;
+		d_in_n: in std_logic;
+		clk_out_p: out std_logic;
+		clk_out_n: out std_logic;
+		d_out_p: out std_logic;
+		d_out_n: out std_logic
+	);
+
+end top;
+
+architecture rtl of top is
+
+	signal sysclk_u, sysclk, clk_u, clk, d_in, da, db: std_logic;
+	signal ctra, ctrb: unsigned(15 downto 0) := X"0000";
+	signal clkout: std_logic;
+
+begin
+
+-- Clock and data in
+
+	ibufg_sysclk: IBUFGDS
+		port map(
+			i => sysclk_p,
+			ib => sysclk_n,
+			o => sysclk_u
+		);
+		
+	bufg_sysclk: BUFG
+		port map(
+			i => sysclk_u,
+			o => sysclk
+		);
+
+	ibufg_clk: IBUFGDS
+		port map(
+			i => clk_in_p,
+			ib => clk_in_n,
+			o => clk_u
+		);
+	
+	bufg_clk: BUFG
+		port map(
+			i => clk_u,
+			o => clk
+		);
+		
+	ibufds_d: IBUFDS
+		port map(
+			i => d_in_p,
+			ib => d_in_n,
+			o => d_in
+		);
+		
+-- IOB registers
+
+	da <= d_in when falling_edge(clk);
+	db <= da when falling_edge(clk);
+	
+-- Clock and data out
+
+	oddr_clk: ODDR
+		port map(
+			q => clkout,
+			c => clk,
+			ce => '1',
+			d1 => '0',
+			d2 => '1',
+			r => '0',
+			s => '0'
+		);
+		
+	obuf_clk: OBUFDS
+		port map(
+			i => clkout,
+			o => clk_out_p,
+			ob => clk_out_n
+		);
+		
+	obuf_d: OBUFDS
+		port map(
+			i => db,
+			o => d_out_p,
+			ob => d_out_n
+		);	
+
+-- Debug counter
+
+	ctra <= ctra + 1 when rising_edge(clk);
+	ctrb <= ctrb + 1 when rising_edge(sysclk);
+
+end rtl;
