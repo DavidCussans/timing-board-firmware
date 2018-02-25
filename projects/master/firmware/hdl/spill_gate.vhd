8b5cf6848267da7adb128ab590cbe1b1005f7ad5
Dave Newbold <dave.newbold@cern.ch>
2018-02-25 17:39:42 +0000

    Adding persistent spill gate

--- projects/master/firmware/hdl/spill_gate.vhd
+++ projects/master/firmware/hdl/spill_gate.vhd
@@ -38,7 +38,8 @@ architecture rtl of spill_gate is
 	signal ectr: unsigned(23 downto 0) := (others => '0');
 	signal cctr: unsigned(7 downto 0) := (others => '0');
 	signal sctr: unsigned(15 downto 0);
-	signal s, spill_i, ctrl_en, ctrl_en_fake: std_logic;
+	signal ctrl_en, ctrl_en_fake, ctrl_force: std_logic;
+	signal spill_i, spill_f, spill_r: std_logic;
 	signal ctrl_fake_cyc_len, ctrl_fake_spill_len: std_logic_vector(7 downto 0);
 
 begin
@@ -59,9 +60,10 @@ begin
 			q => ctrl
 		);
 		
-	stat(0) <= std_logic_vector(sctr) & X"000" & "000" & spill_i;
+	stat(0) <= std_logic_vector(sctr) & X"000" & '0' & spill_r & '0' & spill_i;
 	ctrl_en <= ctrl(0)(0);
 	ctrl_en_fake <= ctrl(0)(1);
+	ctrl_force <= ctrl(0)(2);
 	ctrl_fake_cyc_len <= ctrl(0)(23 downto 16);
 	ctrl_fake_spill_len <= ctrl(0)(31 downto 24);
 
@@ -71,43 +73,63 @@ begin
 	begin
 		if rising_edge(clk) then
 			ectr <= ectr + 1;
-			s <= '0';
 			if rst = '1' or ctrl_en_fake = '0' then
-				spill_i <= '0';
-				sctr <= (others => '0');
 				cctr <= (others => '0');
+				spill_f <= '0';
 			elsif and_reduce(std_logic_vector(ectr)) = '1' then
 				if cctr = unsigned(ctrl_fake_cyc_len) then
 					cctr <= (others => '0');
-					spill_i <= '1';
-					s <= '1';
-					sctr <= sctr + 1;
+					spill_f <= '1';
 				else
 					cctr <= cctr + 1;
-					if cctr = unsigned(ctrl_fake_spill_len) then
-						spill_i <= '0';
-						s <= '1';
+					if spill_f = '1' and cctr = unsigned(ctrl_fake_spill_len) then
+						spill_f <= '0';
 					end if;
 				end if;
 			end if;
 		end if;
 	end process;
 	
-	spill <= spill_i;
+-- Real spills will also get or'ed here
+	
+	spill_r <= spill_f or ctrl_force;	
 
-	process(s, spill_i)
+-- Command generator
+
+	process(clk)
 	begin
-		if s = '1' then
-			if spill_i = '1' then
-				scmd_out.d <= X"0" & SCMD_SPILL_START;
+		if rising_edge(clk) then
+			if rst = '1' or spill_r = spill_i then
+				scmd_out <= CMD_W_NULL;
 			else
-				scmd_out.d <= X"0" & SCMD_SPILL_STOP;
+				if spill_r = '1' and spill_i = '0' then
+					scmd_out.d <= X"0" & SCMD_SPILL_START;
+				else
+					scmd_out.d <= X"0" & SCMD_SPILL_STOP;
+				end if;
+				scmd_out.valid <= '1';
+				scmd_out.last <= '1';
 			end if;
-			scmd_out.valid <= '1';
-			scmd_out.last <= '1';
-		else
-			scmd_out <= CMD_W_NULL;
 		end if;
 	end process;
 
+-- Spill counter
+
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			if rst = '1' then
+				spill_i <= '0';
+				sctr <= (others => '0');
+			elsif scmd_in.ack = '1' then
+				spill_i <= spill_r;
+				if spill_r = '1' then
+					sctr <= sctr + 1;
+				end if;
+			end if;
+		end if;
+	end process;
+	
+	spill <= spill_i;
+	
 end rtl;
