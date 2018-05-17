038eb1d164d9a90f6142376b4a1aeff3d4c3d2e1
Dave Newbold <dave.newbold@cern.ch>
2018-05-17 17:09:14 -0500

    Adding config info to fmc test

--- projects/test/fmc_test/firmware/hdl/fmc_master.vhd
+++ projects/test/fmc_test/firmware/hdl/fmc_master.vhd
@@ -14,6 +14,9 @@ library unisim;
 use unisim.VComponents.all;
 
 entity payload is
+	generic(
+		CARRIER_TYPE: std_logic_vector(7 downto 0)
+	);
 	port(
 		ipb_clk: in std_logic;
 		ipb_rst: in std_logic;
@@ -61,6 +64,8 @@ end payload;
 
 architecture rtl of payload is
 
+	constant DESIGN_TYPE: std_logic_vector := X"00";
+
 	signal ipbw: ipb_wbus_array(N_SLAVES - 1 downto 0);
 	signal ipbr: ipb_rbus_array(N_SLAVES - 1 downto 0);
 	signal ctrl: ipb_reg_v(0 downto 0);
@@ -96,6 +101,10 @@ begin
 -- IO
 
 	io: entity work.pdts_fmc_io
+		generic map(
+			CARRIER_TYPE => CARRIER_TYPE,
+			DESIGN_TYPE => DESIGN_TYPE
+		)
 		port map(
 			ipb_clk => ipb_clk,
 			ipb_rst => ipb_rst,
