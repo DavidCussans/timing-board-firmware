555a19e87010e5de3809a940d7dfe3647615d9e0
Dave Newbold <dave.newbold@cern.ch>
2018-05-23 20:23:58 +0100

    Addrtab update

--- projects/ouroboros/firmware/hdl/ipbus_decode_top.vhd
+++ projects/ouroboros/firmware/hdl/ipbus_decode_top.vhd
@@ -17,7 +17,7 @@ package ipbus_decode_top is
   subtype ipbus_sel_t is std_logic_vector(IPBUS_SEL_WIDTH - 1 downto 0);
   function ipbus_sel_top(addr : in std_logic_vector(31 downto 0)) return ipbus_sel_t;
 
--- START automatically  generated VHDL the Tue Mar  6 17:11:46 2018 
+-- START automatically  generated VHDL the Wed May 23 20:23:04 2018 
   constant N_SLV_IO: integer := 0;
   constant N_SLV_MASTER: integer := 1;
   constant N_SLV_ENDPOINT0: integer := 2;
@@ -36,19 +36,19 @@ package body ipbus_decode_top is
     variable sel: ipbus_sel_t;
   begin
 
--- START automatically  generated VHDL the Tue Mar  6 17:11:46 2018 
-    if    std_match(addr, "---------------------00-00------") then
-      sel := ipbus_sel_t(to_unsigned(N_SLV_IO, IPBUS_SEL_WIDTH)); -- io / base 0x00000000 / mask 0x000006c0
-    elsif std_match(addr, "---------------------01---------") then
-      sel := ipbus_sel_t(to_unsigned(N_SLV_MASTER, IPBUS_SEL_WIDTH)); -- master / base 0x00000200 / mask 0x00000600
-    elsif std_match(addr, "---------------------10-00------") then
-      sel := ipbus_sel_t(to_unsigned(N_SLV_ENDPOINT0, IPBUS_SEL_WIDTH)); -- endpoint0 / base 0x00000400 / mask 0x000006c0
-    elsif std_match(addr, "---------------------10-01------") then
-      sel := ipbus_sel_t(to_unsigned(N_SLV_ENDPOINT1, IPBUS_SEL_WIDTH)); -- endpoint1 / base 0x00000440 / mask 0x000006c0
-    elsif std_match(addr, "---------------------10-10------") then
-      sel := ipbus_sel_t(to_unsigned(N_SLV_ENDPOINT2, IPBUS_SEL_WIDTH)); -- endpoint2 / base 0x00000480 / mask 0x000006c0
-    elsif std_match(addr, "---------------------10-11------") then
-      sel := ipbus_sel_t(to_unsigned(N_SLV_ENDPOINT3, IPBUS_SEL_WIDTH)); -- endpoint3 / base 0x000004c0 / mask 0x000006c0
+-- START automatically  generated VHDL the Wed May 23 20:23:04 2018 
+    if    std_match(addr, "-------------------0-000--------") then
+      sel := ipbus_sel_t(to_unsigned(N_SLV_IO, IPBUS_SEL_WIDTH)); -- io / base 0x00000000 / mask 0x00001700
+    elsif std_match(addr, "-------------------0-10---------") then
+      sel := ipbus_sel_t(to_unsigned(N_SLV_MASTER, IPBUS_SEL_WIDTH)); -- master / base 0x00000400 / mask 0x00001600
+    elsif std_match(addr, "-------------------1-000--------") then
+      sel := ipbus_sel_t(to_unsigned(N_SLV_ENDPOINT0, IPBUS_SEL_WIDTH)); -- endpoint0 / base 0x00001000 / mask 0x00001700
+    elsif std_match(addr, "-------------------1-001--------") then
+      sel := ipbus_sel_t(to_unsigned(N_SLV_ENDPOINT1, IPBUS_SEL_WIDTH)); -- endpoint1 / base 0x00001100 / mask 0x00001700
+    elsif std_match(addr, "-------------------1-010--------") then
+      sel := ipbus_sel_t(to_unsigned(N_SLV_ENDPOINT2, IPBUS_SEL_WIDTH)); -- endpoint2 / base 0x00001200 / mask 0x00001700
+    elsif std_match(addr, "-------------------1-011--------") then
+      sel := ipbus_sel_t(to_unsigned(N_SLV_ENDPOINT3, IPBUS_SEL_WIDTH)); -- endpoint3 / base 0x00001300 / mask 0x00001700
 -- END automatically generated VHDL
 
     else
