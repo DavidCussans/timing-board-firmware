f83ff84c1f18708f7331b79e89f9cd05c439eb69
Alessandro Thea <alessandro.thea@cern.ch>
2018-05-30 08:28:39 +0200

    Version updated to v4a8 (0x040008)

--- projects/endpoint/firmware/hdl/wrapper_defs.vhd
+++ projects/endpoint/firmware/hdl/wrapper_defs.vhd
@@ -9,6 +9,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 package wrapper_defs is
 
-	constant WRAPPER_VERSION: std_logic_vector(31 downto 0) := X"00040007"; -- Version number
+	constant WRAPPER_VERSION: std_logic_vector(31 downto 0) := X"00040008"; -- Version number
 	
 end wrapper_defs;
--- projects/master/firmware/sim_hdl/master_defs.vhd
+++ projects/master/firmware/sim_hdl/master_defs.vhd
@@ -9,7 +9,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 package master_defs is
 
-	constant MASTER_VERSION: std_logic_vector(31 downto 0) := X"00040007"; -- Version number
+	constant MASTER_VERSION: std_logic_vector(31 downto 0) := X"00040008"; -- Version number
 	constant N_PART: integer := 1; -- Number of partitions (max 4 at present)
 	constant N_CHAN: integer := 1; -- Number of scmd generator channels
 	constant TS_DIV: positive := 500;
