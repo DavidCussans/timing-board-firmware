c5a7516c07ab55c324b944ac34aa463c3b75eb94
Dave Newbold <dave.newbold@cern.ch>
2018-06-28 18:05:12 +0200

    Adding tx loopback to sim

--- projects/ouroboros/firmware/sim_hdl/payload_sim.vhd
+++ projects/ouroboros/firmware/sim_hdl/payload_sim.vhd
@@ -98,12 +98,12 @@ begin
 
 -- master block
 
-	master: entity work.master
+	master: entity work.master_top
 		port map(
 			ipb_clk => ipb_clk,
 			ipb_rst => ipb_rst,
-			ipb_in => ipbw(N_SLV_MASTER),
-			ipb_out => ipbr(N_SLV_MASTER),
+			ipb_in => ipbw(N_SLV_MASTER_TOP),
+			ipb_out => ipbr(N_SLV_MASTER_TOP),
 			mclk => fmc_clk,
 			clk => clk,
 			rst => rst,
