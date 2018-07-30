b8ec65a7cea1d2864e984b721591e28512173f00
Dave Newbold <dave.newbold@cern.ch>
2018-07-30 20:03:23 +0100

    Adding to tcl file

--- components/pdts/firmware/ucf/tlu_ax3_pm3.tcl
+++ components/pdts/firmware/ucf/tlu_ax3_pm3.tcl
@@ -5,11 +5,18 @@ create_clock -period 20.000 -name clk [get_ports clk_p]
 
 set_clock_groups -asynchronous -group [get_clocks -include_generated_clocks clk] -group [get_clocks -include_generated_clocks -of_obj [get_pins -of_obj [get_cells infra/clocks/mmcm] -filter {NAME =~ *CLKOUT*}]]
 
-set_property IOSTANDARD LVDS_25 [get_port {clk_p clk_n}]
+set_property IOSTANDARD LVDS_25 [get_port {clk_p clk_n q_sfp_* d_cdr_*}]
 set_property PACKAGE_PIN T5 [get_ports {clk_p}]
 set_property PACKAGE_PIN T4 [get_ports {clk_n}]
 
-set_property IOSTANDARD LVCMOS33 [get_port {q_hdmi_* d_hdmi_* rstb_clk clk_lolb rstb_i2c}]
+set_property IOSTANDARD TMDS_33 [get_port {q_sfp_* d_cdr_*}]
+set_property PACKAGE_PIN F1 [get_ports {q_sfp_p}]
+set_property PACKAGE_PIN E1 [get_ports {q_sfp_n}]
+set_property PACKAGE_PIN J3 [get_ports {d_cdr_p}]
+set_property PACKAGE_PIN J2 [get_ports {d_cdr_n}]
+false_path {q_sfp_* d_cdr_*} sysclk
+
+set_property IOSTANDARD LVCMOS33 [get_port {q_hdmi_* d_hdmi_* rstb_clk clk_lolb rstb_i2c sfp_* cdr_*}]
 set_property PACKAGE_PIN R7 [get_ports {q_hdmi_0}]
 set_property PACKAGE_PIN U4 [get_ports {q_hdmi_1}]
 set_property PACKAGE_PIN R8 [get_ports {q_hdmi_2}]
@@ -18,7 +25,12 @@ set_property PACKAGE_PIN G3 [get_ports {d_hdmi_3}]
 set_property PACKAGE_PIN C1 [get_ports {rstb_clk}]
 set_property PACKAGE_PIN G6 [get_ports {clk_lolb}]
 set_property PACKAGE_PIN C2 [get_ports {rstb_i2c}]
-false_path {q_hdmi_* d_hdmi_* rstb_clk clk_lolb rstb_i2c} sysclk
+set_property PACKAGE_PIN G2 [get_ports {sfp_los}]
+set_property PACKAGE_PIN H2 [get_ports {sfp_fault}]
+set_property PACKAGE_PIN H6 [get_ports {sfp_txdis}]
+set_property PACKAGE_PIN D7 [get_ports {cdr_lol}]
+set_property PACKAGE_PIN E7 [get_ports {cdr_los}]
+false_path {q_hdmi_* d_hdmi_* rstb_clk clk_lolb rstb_i2c sfp_* cdr_*} sysclk
 
 set_property IOSTANDARD LVCMOS25 [get_port {scl sda}]
 set_property PACKAGE_PIN N17 [get_ports {scl}]
