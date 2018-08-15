3883e5f79c2d00cbb2f53830c7741d62f39abdce
Dave Newbold <dave.newbold@cern.ch>
2018-08-15 08:55:16 +0100

    Adding auto sweep to MZ PRBS

--- projects/test/tlu_prbs/firmware/hdl/top_mz.vhd
+++ projects/test/tlu_prbs/firmware/hdl/top_mz.vhd
@@ -29,22 +29,20 @@ architecture rtl of top is
 
 	signal sysclk_u, sysclk, clk_u, clk, d_in, d, dd, d_del, q: std_logic;
 	signal clkout: std_logic;
-	signal vio_rst_u, vio_init_u, vio_edge_u, vio_rst, vio_init, vio_edge, vio_inc: std_logic;
-	signal inc, vio_inc_d: std_logic;
-	signal cyc_ctr, err_ctr, cyc_ctr_r, err_ctr_r: std_logic_vector(47 downto 0);
-	signal zflag, zflag_r: std_logic;
+	signal vio_rst_u, vio_rst: std_logic;
+	signal ctr: unsigned(22 downto 0);
+	signal edge, load, init, copy, copy_d, copy_s, copy_sd: std_logic;
+	signal cyc_ctr, err_ctr, cyc_ctr_r, err_ctr_r, cyc_ctr_p, err_ctr_p: std_logic_vector(47 downto 0);
+	signal zflag, zflag_p, zflag_r: std_logic;
 	signal cntout: std_logic_vector(4 downto 0);
 	
 	attribute MARK_DEBUG: string;
-	attribute MARK_DEBUG of cyc_ctr_r, err_ctr_r, zflag_r, cntout: signal is "TRUE";
+	attribute MARK_DEBUG of cyc_ctr_r, err_ctr_r, zflag_r, cntout, edge, copy_sd: signal is "TRUE";
 	
 	COMPONENT vio_0
 		PORT (
 			clk : IN STD_LOGIC;
-			probe_out0 : OUT STD_LOGIC_VECTOR(0 DOWNTO 0);
-			probe_out1 : OUT STD_LOGIC_VECTOR(0 DOWNTO 0);
-			probe_out2 : OUT STD_LOGIC_VECTOR(0 DOWNTO 0);
-			probe_out3 : OUT STD_LOGIC_VECTOR(0 DOWNTO 0)
+			probe_out0 : OUT STD_LOGIC_VECTOR(0 DOWNTO 0)
 		);
 	END COMPONENT;
 
@@ -84,6 +82,44 @@ begin
 			ib => d_in_n,
 			o => d_in
 		);
+
+-- VIO control
+
+	vio: vio_0
+		port map(
+	    clk => sysclk,
+	    probe_out0(0) => vio_rst_u
+	   );
+	   
+	synchro: entity work.pdts_synchro
+		generic map(
+			N => 1
+		)
+		port map(
+			clk => sysclk,
+			clks => clk,
+			d(0) => vio_rst_u,
+			q(0) => vio_rst
+		);
+
+-- Sweep control
+
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			if vio_rst = '1' then
+				ctr <= (others => '0');
+			else
+				ctr <= ctr + 1;
+			end if;
+		end if;
+	end process;
+	
+	copy <= ctr(16);
+	ld <= ctr(16) and ctr(15) when rising_edge(clk);
+	load <= ctr(16) and not ld;
+	init <= ctr(16) and ctr(15) and ctr(14);
+	edge <= ctr(22);
 		
 -- IOB registers
 
@@ -94,11 +130,11 @@ begin
 		port map(
 			c => sysclk,
 			regrst => '0',
-			ld => vio_rst,
-			ce => inc,
+			ld => load,
+			ce => '0',
 			inc => '1',
 			cinvctrl => '0',
-			cntvaluein => "00000",
+			cntvaluein => ctr(21 downto 17),
 			idatain => d_in,
 			datain => '0',
 			ldpipeen => '0',
@@ -107,7 +143,6 @@ begin
 		);
 		
 	vio_inc_d <= vio_inc when rising_edge(sysclk);
-	inc <= vio_inc and not vio_inc_d;
 
 	iddr: IDDR
 		generic map(
@@ -123,7 +158,7 @@ begin
 			s => '0'
 		);
 		
-	d <= d_in_r when vio_edge = '0' else d_in_f;
+	d <= d_in_r when edge = '0' else d_in_f;
 	dd <= d when rising_edge(clk);
 	q <= dd when rising_edge(clk);
 	
@@ -154,51 +189,52 @@ begin
 			ob => d_out_n
 		);
 		
--- VIO control
-
-	vio: vio_0
-		port map(
-	    clk => sysclk,
-	    probe_out0(0) => vio_rst_u,
-	    probe_out1(0) => vio_init_u,
-	    probe_out2(0) => vio_edge_u,
-	    probe_out3(0) => vio_inc    
-	   );
-	   
-	synchro: entity work.pdts_synchro
-		generic map(
-			N => 3
-		)
-		port map(
-			clk => sysclk,
-			clks => clk,
-			d(0) => vio_rst_u,
-			d(1) => vio_init_u,
-			d(2) => vio_edge_u,
-			q(0) => vio_rst,
-			q(1) => vio_init,
-			q(2) => vio_edge
-		);
-	
 -- PRBS check
 
 	prbs_chk: entity work.prbs7_chk
 		port map(
 			clk => clk,
 			rst => vio_rst,
-			init => vio_init,
+			init => init,
 			d => dd,
 			err_ctr => err_ctr,
 			cyc_ctr => cyc_ctr,
 			zflag => zflag
 		);	
 
+-- Sync back to slow clock
+
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			copy_d <= copy;
+			if copy = '1' and and copy_d = '0' then
+				cyc_ctr_p <= cyc_ctr;
+				err_ctr_p <= err_ctr;
+				zflag_p <= zflag;
+			end if;
+		end if;
+	end process;
+
+	syncb: entity work.pdts_synchro
+		generic map(
+			N => 1
+		)
+		port map(
+			clk => clk,
+			clks => sysclk,
+			d(0) => copy,
+			q(0) => copy_s
+		);
+		
 	process(sysclk)
 	begin
 		if rising_edge(sysclk) then
-			cyc_ctr_r <= cyc_ctr; -- SHonky as hell
-			err_ctr_r <= err_ctr;
-			zflag_r <= zflag;
+			copy_sd <= copy_s;
+			if copy_s = '1' and copy_sd = '0' then
+				cyc_ctr_r <= cyc_ctr_p;
+				err_ctr_r <= err_ctr_p;
+				zflag_r <= zflag_p;
 		end if;
 	end process;
 		
