a6e03621c256905ed37e9e62137c63799bd1a269
Alessandro Thea <alessandro.thea@cern.ch>
2018-09-05 10:58:19 +0200

    Firmware version updated: v4b4

--- projects/master/firmware/hdl/master_defs.vhd
+++ projects/master/firmware/hdl/master_defs.vhd
@@ -9,7 +9,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 package master_defs is
 
-	constant MASTER_VERSION: std_logic_vector(31 downto 0) := X"00040103"; -- Version number
+	constant MASTER_VERSION: std_logic_vector(31 downto 0) := X"00040104"; -- Version number
 	constant N_PART: integer := 4; -- Number of partitions (max 4 at present)
 	constant N_CHAN: integer := 5; -- Number of scmd generator channels
 	constant TS_DIV: positive := 50000000; -- 1Hz (must be divisible by 25)
