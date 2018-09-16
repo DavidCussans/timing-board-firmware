bee5be7779d6edc337fc58fd08ebfecae6480697
Dave Newbold <dave.newbold@cern.ch>
2018-09-16 20:27:23 +0100

    async tx fix

--- projects/master/firmware/hdl/acmd_master.vhd
+++ projects/master/firmware/hdl/acmd_master.vhd
@@ -88,21 +88,18 @@ begin
 			if rst = '1' then
 				c <= "00";
 			elsif acmd_in_i(1).ren = '1' then
-				if c = "10" then
-					c <= "00";
-				else
-					c <= c + 1;
-				end if;
+				c <= c + 1;
 			end if;
 		end if;
 	end process;
 	
 	with c select acmd_out_i(1).d <=
 		ctrl(0)(15 downto 8) when "00",
-		ctrl(0)(23 downto 16) when "01",
+		X"00" when "01",
+		ctrl(0)(23 downto 16) when "10",
 		ctrl(0)(31 downto 24) when others;
 	
-	acmd_out_i(1).last <= '1' when c = "10" else '0';
+	acmd_out_i(1).last <= '1' when c = "11" else '0';
 	acmd_out_i(1).req <= pend;
 	
 -- Arbitrator
