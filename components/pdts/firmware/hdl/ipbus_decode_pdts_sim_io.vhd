5d72f653a824c0b3fff2082a39f2b9270101223d
Dave Newbold <dave.newbold@cern.ch>
2018-09-16 08:09:35 +0100

    Addrtab update

--- components/pdts/firmware/hdl/ipbus_decode_pdts_sim_io.vhd
+++ components/pdts/firmware/hdl/ipbus_decode_pdts_sim_io.vhd
@@ -13,11 +13,11 @@ use ieee.numeric_std.all;
 
 package ipbus_decode_pdts_sim_io is
 
-  constant IPBUS_SEL_WIDTH: positive := 5; -- Should be enough for now?
+  constant IPBUS_SEL_WIDTH: positive := 2;
   subtype ipbus_sel_t is std_logic_vector(IPBUS_SEL_WIDTH - 1 downto 0);
   function ipbus_sel_pdts_sim_io(addr : in std_logic_vector(31 downto 0)) return ipbus_sel_t;
 
--- START automatically  generated VHDL the Sat Apr 21 09:28:47 2018 
+-- START automatically  generated VHDL the Sat Sep 15 16:12:55 2018 
   constant N_SLV_CSR: integer := 0;
   constant N_SLV_CONFIG: integer := 1;
   constant N_SLAVES: integer := 2;
@@ -32,7 +32,7 @@ package body ipbus_decode_pdts_sim_io is
     variable sel: ipbus_sel_t;
   begin
 
--- START automatically  generated VHDL the Sat Apr 21 09:28:47 2018 
+-- START automatically  generated VHDL the Sat Sep 15 16:12:55 2018 
     if    std_match(addr, "------------------------------0-") then
       sel := ipbus_sel_t(to_unsigned(N_SLV_CSR, IPBUS_SEL_WIDTH)); -- csr / base 0x00000000 / mask 0x00000002
     elsif std_match(addr, "------------------------------1-") then
